a692204afbd23177bd26ccf81b3a65f43b2d5efe
thumuvikram <thumuvikram@gmail.com>
2018-09-10 18:33:11 +0530

    Jenkins install script

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -4,6 +4,16 @@
 
 # Script : to install jenkins on the machine using jenkins.war
 
-cd ~/jenkins
-java -jar jenkins.war
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum install jenkins
+sudo service jenkins start
+sudo chkconfig jenkins on
+firewall-cmd --permanent --new-service=jenkins
+firewall-cmd --permanent --service=jenkins --set-short="Jenkins Service Ports"
+firewall-cmd --permanent --service=jenkins --set-description="Jenkins service firewalld port exceptions"
+firewall-cmd --permanent --service=jenkins --add-port=8080/tcp
+firewall-cmd --permanent --add-service=jenkins
+firewall-cmd --zone=public --add-service=http --permanent
+firewall-cmd --reload
 
