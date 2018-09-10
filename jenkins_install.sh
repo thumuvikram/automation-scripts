0b1504d3bb60276a36415ac3fae3335abb62a6c3
thumuvikram <thumuvikram@gmail.com>
2018-09-10 18:39:02 +0530

    Updated script

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -6,7 +6,7 @@
 
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum install jenkins
+sudo yum install -y jenkins
 sudo service jenkins start
 sudo chkconfig jenkins on
 firewall-cmd --permanent --new-service=jenkins
