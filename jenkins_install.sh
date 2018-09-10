4285fdb1a9fbaade3e088941ab47f1dc3eb458a3
thumuvikram <thumuvikram@gmail.com>
2018-09-10 18:44:13 +0530

    yum update added

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -3,7 +3,7 @@
 # filename : jenkins_install.sh
 
 # Script : to install jenkins on the machine RHEL
-
+sudo yum -y update
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum install -y jenkins
