8def890c13e6edca098497b0edff078237fc3b98
thumuvikram <thumuvikram@gmail.com>
2018-09-10 18:33:29 +0530

    Update jenkins_install.sh

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -2,7 +2,7 @@
 
 # filename : jenkins_install.sh
 
-# Script : to install jenkins on the machine using jenkins.war
+# Script : to install jenkins on the machine RHEL
 
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
